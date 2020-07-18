0b208d8a7bfbcbc462cafe16d689d4ed4add99b9
Jegadeeswaran <jegadeesiosdev@gmail.com>
2020-07-18 19:04:04 +0530

    #. Removed mail storyboard, SceneDelegate and ViewController Files. #. Changed App Targeted version as 11.0

--- TechMPOC/AppDelegate.swift
+++ TechMPOC/AppDelegate.swift
@@ -11,27 +11,24 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        addRootViewController()
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    /*
+     First view controller of our app.
+    */
+    private func addRootViewController() {
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        let about = AboutTableViewController()
+        window!.rootViewController = about
+        window!.makeKeyAndVisible()
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
 
 }
 
